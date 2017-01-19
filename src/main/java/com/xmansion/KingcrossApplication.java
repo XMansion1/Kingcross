1c17019ff5879c528d4e53c652fe4940db5858d4
anunay <anunay@anunay>
2017-01-19 15:17:42 +0530

    Streamlining the module

--- src/main/java/com/xmansion/KingcrossApplication.java
+++ src/main/java/com/xmansion/KingcrossApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class KingcrossApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KingcrossApplication.class, args);
 	}
--- src/main/java/com/xmansion/kingcross/Controllers/BaseController.java
+++ src/main/java/com/xmansion/kingcross/Controllers/BaseController.java
@@ -1,35 +1,59 @@
 package com.xmansion.kingcross.Controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+
 /**
  * Created by [)/\ on 29/10/16.
  */
+
 @RestController
 @Slf4j
 public class BaseController {
 
-	@RequestMapping(value = "/echo", method = RequestMethod.GET)
-	public ResponseEntity<String> echo() {
-		log.info("Grounds are not all that secured.");
-		log.debug("Hagrid is watching over, but shhhhhhh!!!!!!!");
-		log.error("Ahhh!!! there is no rule breaking here");
-		log.trace("Mischief Managed!");
-		log.warn("Dumbledore might still be aware of all are mischief");
-		String responseText = "Harry Potter" + " says howdy !!!!";
-		return new ResponseEntity<String>(
-				responseText, HttpStatus.OK);
+	@Autowired
+	HttpServletRequest httpServletRequest;
+
+	private String getHeaderDetails() {
+
+		String responseText = "Welcome to Kingcross.\n" +
+				"This is where our journey starts.\n" +
+				"This application is in Spring Boot 1.4.0 with Java 8.\n" +
+				"This application is here to demonstrate Keycloak Integration and Kong Usage.\n" +
+				"This application requires you to have two roles Role_1 and Role_2 and Role_3. Sorry for my maths." +
+				" \n\n\n\n\n";
+		StringBuilder stringBuilder = new StringBuilder(responseText);
+
+		Collections
+				.list(httpServletRequest.getHeaderNames())
+				.stream()
+				.forEach(header->stringBuilder
+						.append("\n" + header + "\t" + httpServletRequest.getHeader(header) )
+				);
+		return  stringBuilder.toString();
 	}
 
-	@RequestMapping(value = "/secured_echo", method = RequestMethod.GET)
+	@RequestMapping(value = "/echo", method = RequestMethod.GET)
+	public ResponseEntity<String> echo() {
+		return new ResponseEntity<String>(
+				getHeaderDetails(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/secured-echo", method = RequestMethod.GET)
 	public ResponseEntity<String> securedEcho() {
-		log.info("We are in the secured world");
-		String responseText = "Potter & Dumbledore" + " says howdy !!!!";
+
+		String responseText = "\n\t--------------Secured Page--------------\n\n"
+				+getHeaderDetails();
 		return new ResponseEntity<String>(
 				responseText, HttpStatus.OK);
 	}
